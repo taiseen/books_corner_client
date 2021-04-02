8a4d0ac884852a43f826d46997aecd178f0be8ea
taiseen <taiseen.cse@gmail.com>
2021-04-02 22:30:22 +0600

    Firebase Error

--- src/Components/FireBase/FireBaseConfig.js
+++ src/Components/FireBase/FireBaseConfig.js
@@ -1,10 +1,10 @@
 const FireBaseConfig = {
-  apiKey: "AIzaSyBRI0h1WeaYwMcf8zGIkyKgO9LthBslX9Q",
-  authDomain: "books-corner-bd.firebaseapp.com",
-  projectId: "books-corner-bd",
-  storageBucket: "books-corner-bd.appspot.com",
-  messagingSenderId: "244475351750",
-  appId: "1:244475351750:web:a2b17d69ca4c5a152fbdc7"
+  apiKey: "AIzaSyDVqyBRlVMjAOrc8eXe7LDNig8RnuPKvdE",
+  authDomain: "development-books-corner-19156.firebaseapp.com",
+  projectId: "development-books-corner-19156",
+  storageBucket: "development-books-corner-19156.appspot.com",
+  messagingSenderId: "559860248009",
+  appId: "1:559860248009:web:3f1ab1499e0c5e30d00c18"
 };
 
 export default FireBaseConfig;
